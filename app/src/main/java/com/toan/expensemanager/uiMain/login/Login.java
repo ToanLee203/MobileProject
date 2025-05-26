6c014da6f069d92ff2322aa290a36cf2a84d9bc0
ToanLV <toanlvhe173348@fpt.edu.vn>
2025-05-26 12:04:18 +0700

    Commit fist Mobile

---
+++ app/src/main/java/com/toan/expensemanager/uiMain/login/Login.java
@@ -0,0 +1,35 @@
+package com.toan.expensemanager.uiMain.login;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.toan.expensemanager.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Gán nút và xử lý sự kiện nhấn
+        Button loginButton = findViewById(R.id.buttonLogin);
+        loginButton.setOnClickListener(v -> {
+            Toast.makeText(this, "Đăng nhập demo!", Toast.LENGTH_SHORT).show();
+        });
+    }
+}
